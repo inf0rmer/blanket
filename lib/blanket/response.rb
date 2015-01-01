adc182e2ea4d747005c0f52d9fd3749c3c47e515
Damir Svrtan <damir.svrtan@gmail.com>
2015-01-01 13:45:52 +0100

    No need to check if json is nil since initialize handles it.

--- lib/blanket/response.rb
+++ lib/blanket/response.rb
@@ -28,13 +28,11 @@ module Blanket
     end
 
     def payload_from_json(json)
-      if json
-        parsed = [json].flatten.map do |item|
-          RecursiveOpenStruct.new item, recurse_over_arrays: true
-        end
-
-        (parsed.count == 1) ? parsed.first : parsed
+      parsed = [json].flatten.map do |item|
+        RecursiveOpenStruct.new item, recurse_over_arrays: true
       end
+
+      (parsed.count == 1) ? parsed.first : parsed
     end
 
   end
